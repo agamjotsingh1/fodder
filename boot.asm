8fa5a16aefda209cab939e65465eb95c6b6bdcf2
agamjotsingh1 <agamjot.s.kohli@gmail.com>
2025-06-25 18:14:47 +0530

    commit

---
+++ boot.asm
@@ -0,0 +1,38 @@
+ORG 0
+BITS 16
+
+start:
+    cli ; Clears interrupts
+
+    ; Segmentation
+    mov ax, 0x7c0
+    mov ds, ax
+    mov es, ax
+
+    sti ; Enable interrupts
+    mov si, message
+    call print
+    jmp $
+
+print:
+    mov bx, 0
+
+.loop:
+    lodsb
+    cmp al, 0
+    je .done
+    call print_char
+    jmp .loop
+
+.done:
+    ret
+
+print_char:
+    mov ah, 0eh
+    int 0x10
+    ret
+
+message: db 'Hello World!', 0
+
+times 510-($ - $$) db 0
+dw 0xAA55

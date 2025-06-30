8eb5499f702052889f3165eb0f0bbfb6d21572cc
agamjotsingh1 <agamjot.s.kohli@gmail.com>
2025-06-30 19:19:34 +0530

    commit

--- src/idt/idt.h
+++ src/idt/idt.h
@@ -1,12 +1,14 @@
 #ifndef IDT_H
 #define IDT_H
 
+#include <stdint.h>
+
 struct idt_desc {
-    uint16_t offset1; // Offset bits 0-15
+    uint16_t offset_1; // Offset bits 0-15
     uint16_t selector; // Selector (in GDT)
     uint16_t zero; // Null space, does nothing
     uint16_t type_attr; // Descriptor types and attributes
-    uint16_t offset2; // Offset bits 0-15
+    uint16_t offset_2; // Offset bits 0-15
 } __attribute__((packed));
 
 struct idtr_desc {
